b8c6194e9b1f4c6a01b14c62d1607eb207e7f640
sm5734 <sm5734@srmist.edu.in>
2026-03-01 13:49:40 +0530

    Delete level1 directory

--- level1/q2/q2.java
+++
@@ -1,7 +0,0 @@
-public class AverageMark{
-    public static void main(String[] args) {
-        int math=94,phy=95,chem=96;
-        double average=(math+phy+chem)/3.0;
-        System.out.println("Sam's average in PCM is "+average);
-    }
-}
--- level1/q3/q3.java
+++
@@ -1,7 +0,0 @@
-public class KmtoMile{
-    public static void main(String[] args) {
-        double DistInKm=10.8,parameter=1.6;
-        double DistInMile=DistInKm/parameter;
-        System.out.println("The distance "+DistInKm+" km in miles is "+DistInMile);
-    }
-}
