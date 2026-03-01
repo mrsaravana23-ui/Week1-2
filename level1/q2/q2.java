5fb492bd27f00a4c74b35f6d5471768186d29b68
sm5734 <sm5734@srmist.edu.in>
2026-03-01 14:01:45 +0530

    level1:Java code for Height Converter

---
+++ level1/q2/q2.java
@@ -0,0 +1,7 @@
+public class AverageMark{
+    public static void main(String[] args) {
+        int math=94,phy=95,chem=96;
+        double average=(math+phy+chem)/3.0;
+        System.out.println("Sam's average in PCM is "+average);
+    }
+}
---
+++ level1/q4/q4.java
@@ -0,0 +1,8 @@
+public class Profit{
+    public static void main(String[] args) {
+        double sp=191,cp=129;
+        double profit =sp-cp;
+        double ProfitPercentage= (profit/cp)*100;
+        System.out.println("The Cost Price is INR "+cp+" and Selling Price is INR "+sp+ "\nThe Profit is INR "+profit+" and the Profit Percentage is "+ProfitPercentage);
+    }
+}
