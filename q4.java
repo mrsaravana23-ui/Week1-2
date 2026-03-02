9280a90294b9dd045ac759b43d1559256df4707c
sm5734 <sm5734@srmist.edu.in>
2026-03-02 08:35:24 +0530

    q4:feettoyardmile

---
+++ q4.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class FeetToYardMile{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Distance in Feet : ");
+        double feet=sc.nextDouble();
+        double parameter1=3,parameter2=1760;
+        double yard=feet/parameter1;
+        double mile=yard/parameter2;
+        System.out.println("The distance in yards is "+yard+" while the distance in miles is "+mile);
+    }
+}
