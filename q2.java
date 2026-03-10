d9c35b84494538187546b6dfb2645d447d78135c
sm5734 <sm5734@srmist.edu.in>
2026-03-10 08:45:10 +0530

    temperatureconversion

---
+++ q2.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class TemperatureConversion{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Temperature in degree Farenheit : ");
+        double farenheit=sc.nextDouble();
+        double celsiusResult=(farenheit-32.0)*(5.0/9.0);
+        System.out.println("The "+farenheit+" farenheit is "+celsiusResult+" celsius");
+    }
+}
